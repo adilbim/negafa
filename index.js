a141df22f87e9306165ba47b9985ae6ef65c12f6
adil bimzagh <abshow15@gmail.com>
2019-08-19 20:38:43 +0100

    this is it

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.get("/grid", function(req, res){
     res.render("gridpage.ejs");
 });
 
-app.listen(5000, function(){
-    console.log("server ahas started succesfully!");
-});
+// app.listen(5000, function(){
+//     console.log("server ahas started succesfully!");
+// });
 
-// if (port == null || port == "") {
-//     port = 8000;
-//   }
-//   app.listen(port);
+if (port == null || port == "") {
+    port = 8000;
+  }
+  app.listen(port);
